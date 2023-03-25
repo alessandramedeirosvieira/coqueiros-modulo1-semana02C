6120811f65e9ece8753f895b5477e7d3998a0bb4
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-03-24 23:46:00 -0300

    add

---
+++ EXERCICIO2/Program.cs
@@ -0,0 +1,12 @@
+﻿using EXERCICIO2;
+
+    FichaInscricao ficha = new FichaInscricao();
+    ficha.Id = 1;
+    ficha.Nome = "Maria";
+    ficha.DataNascimento = new DateTime(2023, 2, 3);
+    ficha.MenorIdade = true;
+
+    ficha.ImprimirDados();
+
+    // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
